d861689bba83836210f2559b16d7a35969dc57f7
Dmitry Zuikov <dzuikov@gmail.com>
2023-01-10 09:20:05 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+description = "hbs2";
+
+inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    # haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
+    haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils/896219e5bde6efac72198550454e9dd9b5ed9ac9";
+    hspup.url = "github:voidlizard/hspup";
+    hspup.inputs.nixpkgs.follows = "nixpkgs";
+};
+
+outputs = { self, nixpkgs, haskell-flake-utils, ... }@inputs:
+
+ haskell-flake-utils.lib.simpleCabalProject2flake {
+   inherit self nixpkgs;
+   systems = [ "x86_64-linux" ];
+   name = "hbs2-core";
+
+   packageNames = [
+     "hbs2-core"
+     "hbs2-storage-simple"
+   ];
+
+   packageDirs = {
+     "hbs2-core" = "./hbs2-core";
+     "hbs2-storage-simple" = "./hbs2-storage-simple";
+   };
+
+
+   packagePostOverrides = { pkgs }: with pkgs; with haskell.lib; [
+    disableExecutableProfiling
+    disableLibraryProfiling
+    dontBenchmark
+    dontCoverage
+    dontDistribute
+    dontHaddock
+    dontHyperlinkSource
+    doStrip
+    enableDeadCodeElimination
+    justStaticExecutables
+
+    dontCheck
+   ];
+
+   shellExtBuildInputs = {pkgs}: with pkgs;  [
+     haskellPackages.haskell-language-server
+     inputs.hspup.packages.${pkgs.system}.default
+   ];
+
+ };
+
+
+}
