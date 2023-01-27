2525f1abb04e68f50a5439b41de6c403321a94e1
Dmitry Zuikov <dzuikov@gmail.com>
2023-01-27 11:24:53 +0300

    ...

--- hbs2-tests/test/TestChunkWriter.hs
+++ hbs2-tests/test/TestChunkWriter.hs
@@ -52,7 +52,7 @@ main = do
 
     storage <- simpleStorageInit opts :: IO (SimpleStorage HbSync)
 
-    w1 <- replicateM 1 $ async (simpleStorageWorker storage)
+    w1 <- replicateM 128 $ async (simpleStorageWorker storage)
 
     cw <- newChunkWriterIO @HbSync storage (Just (dir </> ".qqq"))
 
