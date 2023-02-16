5af01af0900d85100adf5c4ce26b0d635ce2199c
Dmitry Zuikov <dzuikov@gmail.com>
2023-02-16 14:09:02 +0300

    wip HEsUhP3CJN

--- hbs2-peer/app/BlockDownload.hs
+++ hbs2-peer/app/BlockDownload.hs
@@ -382,7 +382,7 @@ updatePeerInfo onError pinfo = do
                             let buM = headMay $ drop 2 $ IntSet.toDescList buSet
                             writeTVar (view peerBurstMax pinfo) buM
                             -- let s = IntSet.size buSet
-                            let buN = defBurst -- atDef defBurst (IntSet.toList buSet) (s `div` 2 )
+                            let buN =  headDef defBurst $ drop 10 $ IntSet.toDescList buSet
                             pure (buN, trimDown 50 $ IntSet.insert buN buSet)
 
 
