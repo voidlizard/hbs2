bc1ffa652b57b00399aed149e191b0135388f863
Dmitry Zuikov <dzuikov@gmail.com>
2023-02-17 15:09:15 +0300

    wip

--- hbs2-core/lib/HBS2/Net/Proto/Ref.hs
+++ hbs2-core/lib/HBS2/Net/Proto/Ref.hs
@@ -1,23 +1,31 @@
 {-# Language TemplateHaskell #-}
+{-# Language UndecidableInstances #-}
 module HBS2.Net.Proto.Ref where
 
 import HBS2.Prelude.Plated
 import HBS2.Data.Types.Refs (HashRef)
+import HBS2.Net.Auth.Credentials
 
+import Data.ByteString (ByteString)
 import Lens.Micro.Platform
 import Codec.Serialise
 import Data.Set (Set)
 
-data Ref = Ref
-  { _refACB  :: HashRef
-  , _refData :: HashRef
+data Ref e = Ref
+  { _refNonce :: ByteString
+  , _refSign  :: Signature e
+  , _refACB   :: HashRef
+  , _refData  :: HashRef
   }
-  deriving stock (Generic,Eq)
+  deriving stock (Generic)
 
 makeLenses 'Ref
 
 
-instance Serialise Ref
+type IsRef e = (Eq (Signature e), Serialise (Signature e))
 
-newtype RefSet = RefSet (Set Ref)
+deriving stock instance IsRef e => Eq (Ref e)
+
+instance IsRef e => Serialise (Ref e)
+
 
