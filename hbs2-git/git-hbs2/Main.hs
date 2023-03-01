fc3e59f1d663e6b0b440807cbed02ff933c9f94d
Dmitry Zuikov <dzuikov@gmail.com>
2023-03-01 06:18:37 +0300

    wip

--- hbs2-git/git-hbs2/Main.hs
+++ hbs2-git/git-hbs2/Main.hs
@@ -2,6 +2,14 @@ module Main where
 
 import qualified MyLib (someFunc)
 
+-- TODO: hbs2-git-state-init
+--   Создать / обновить стейт в XDG_DATA_HOME/.hbs2-git, включая БД
+--   Обновление стейта есть лог операций
+
+-- TODO: hbs2-git-state-log
+--   Определить операции над стейтом
+
+
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
