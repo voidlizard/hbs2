feb7dbca72cc8aaf65ea8b73d7f91694df5fe33d
Dmitry Zuikov <dzuikov@gmail.com>
2023-03-01 10:30:44 +0300

    wip

--- hbs2-git/git-hbs2/HBS2Git/Config.hs
+++ hbs2-git/git-hbs2/HBS2Git/Config.hs
@@ -2,6 +2,7 @@ module HBS2Git.Config where
 
 import HBS2.Prelude
 import HBS2.System.Logger.Simple
+import HBS2.OrDie
 
 import Data.Config.Suckless
 
@@ -9,10 +10,22 @@ import System.FilePath
 import System.Directory
 import System.IO
 
-configPath :: MonadIO m => m FilePath
-configPath = do
+
+findGitDir :: FilePath -> IO (Maybe FilePath)
+findGitDir dir = do
+  let gitDir = dir </> ".git"
+  exists <- doesDirectoryExist gitDir
+  if exists
+    then return $ Just gitDir
+    else let parentDir = takeDirectory dir
+         in if parentDir == dir -- we've reached the root directory
+               then return Nothing
+               else findGitDir parentDir
+
+
+configPath :: MonadIO m => FilePath -> m FilePath
+configPath pwd = do
   xdg <- liftIO $ getXdgDirectory XdgConfig "hbs2-git"
-  pwd <- liftIO (getCurrentDirectory >>= canonicalizePath)
   home <- liftIO getHomeDirectory
   pure $ xdg </> makeRelative home pwd
 
@@ -20,8 +33,17 @@ configInit :: MonadIO m => m ()
 configInit = liftIO do
   trace "configInit"
 
-  confP <- configPath
+  trace "locating .git directory"
 
+  this <- getCurrentDirectory
+
+  gitDir <- findGitDir this `orDie` ".git directory not found"
+
+  let pwd = takeDirectory gitDir
+
+  confP <- configPath pwd
+
+  trace $ "git dir" <+> pretty gitDir
   trace $ "confPath:" <+> pretty confP
 
   here <- doesDirectoryExist confP
--- hbs2-git/git-hbs2/HBS2Git/Export.hs
+++ hbs2-git/git-hbs2/HBS2Git/Export.hs
@@ -1,10 +1,22 @@
 module HBS2Git.Export where
 
 import HBS2.Prelude
+import HBS2.System.Logger.Simple
 
 runExport :: MonadIO m => m ()
 runExport = liftIO do
-  print $ pretty "Export"
+  trace "Export"
+
+  -- TODO: read-repo-head
+  trace "read repository head"
+
+  -- TODO: create-repo-head-block
+  trace "create-repo-head-block"
+
+  -- TODO: build-transitive-closure
+  trace "build-transitive-closure"
+
+
   pure ()
 
 
