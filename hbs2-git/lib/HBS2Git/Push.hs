d762a808d2969b368a193a03c842e985633b9af5
Dmitry Zuikov <dzuikov@gmail.com>
2023-03-05 07:30:23 +0300

    wip. git push

---
+++ hbs2-git/lib/HBS2Git/Push.hs
@@ -0,0 +1,22 @@
+module HBS2Git.Push where
+
+import HBS2.Prelude.Plated
+import HBS2.Data.Types.Refs
+-- import HBS2.OrDie
+-- import HBS2.System.Logger.Simple
+-- import HBS2.Merkle
+-- import HBS2.Hash
+
+
+import HBS2.Git.Local
+-- import HBS2.Git.Local.CLI
+
+-- import HBS2Git.App
+-- import HBS2Git.State
+--
+--
+
+
+push :: MonadIO m => GitRef -> HashRef -> m ()
+push branch remote = do
+  pure ()
