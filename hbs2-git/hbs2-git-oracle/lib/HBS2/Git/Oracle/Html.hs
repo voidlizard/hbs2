b17c20d980f27ca703ca1331964150913975f590
Dmitry Zuikov <dzuikov@gmail.com>
2024-03-29 08:16:09 +0300

    wip

--- hbs2-git/hbs2-git-oracle/lib/HBS2/Git/Oracle/Html.hs
+++ hbs2-git/hbs2-git-oracle/lib/HBS2/Git/Oracle/Html.hs
@@ -5,13 +5,27 @@ import HBS2.Git.Oracle.State
 
 import Data.HashMap.Strict (HashMap)
 
-import Lucid (Html,HtmlT,toHtml,renderBS)
+import Lucid (Html,HtmlT,toHtml,toHtmlRaw,renderBS)
 import Lucid.Html5 hiding (for_)
 
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.ByteString.Lazy
-import Control.Monad.Identity
+import Text.Pandoc
+import Text.Pandoc.Error (handleError)
+
+
+markdownToHtml :: Text -> Either PandocError String
+markdownToHtml markdown = runPure $ do
+  doc <- readMarkdown def {readerExtensions = pandocExtensions} markdown
+  html <- writeHtml5String def {writerExtensions = pandocExtensions} doc
+  return $ Text.unpack html
+
+renderMarkdown :: Text -> Html ()
+renderMarkdown markdown = case markdownToHtml markdown of
+    Left{} -> mempty
+    Right html -> toHtmlRaw $ Text.pack html
+
 
 renderEntries :: Monad m => HashMap Text Text -> [(HashVal, Text, Text)] -> m ByteString
 renderEntries _ items = pure $ renderBS do
@@ -25,8 +39,7 @@ renderEntries _ items = pure $ renderBS do
 
             when ( Text.length n > 2) do
               h3_ [class_ "repo-name"] (toHtml (show $ pretty n))
-              span_ [class_ "repo-reference"] (toHtml (show $ pretty h))
-
-            -- td_ (toHtml (show $ pretty n))
-            -- td_ (toHtml (show $ pretty b))
+              div_ [class_ "repo-reference"] (toHtml (show $ pretty h))
+              div_ [class_ "repo-brief"] do
+                renderMarkdown b
 
