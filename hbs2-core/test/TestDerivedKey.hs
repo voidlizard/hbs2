ceeb498505ec0b036d51e3379464807a86285471
Dmitry Zuikov <dzuikov@gmail.com>
2024-04-15 06:43:46 +0300

    tests fixed

--- hbs2-core/test/TestDerivedKey.hs
+++ hbs2-core/test/TestDerivedKey.hs
@@ -23,7 +23,7 @@ testDerivedKeys1 = do
   let nonce = 0x123456780928934 :: Word64
   (pk1,sk1) <- derivedKey @'HBS2Basic @'Sign nonce sk
 
-  let box = makeSignedBox @L4Proto pk1 sk1 (42 :: Word32)
+  let box = makeSignedBox @'HBS2Basic pk1 sk1 (42 :: Word32)
 
   (pk, n) <- pure (unboxSignedBox0 box)
               `orDie` "can not unbox"
