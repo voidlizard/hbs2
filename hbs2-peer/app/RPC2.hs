1baddb419c6d13d4aa97203b4be4a336ca76828f
voidlizard <dzuikov@gmail.com>
2024-11-04 08:04:21 +0300

    request-block-size

--- hbs2-peer/app/RPC2.hs
+++ hbs2-peer/app/RPC2.hs
@@ -11,13 +11,16 @@ module RPC2
 
 
 import HBS2.Prelude.Plated
+import HBS2.Events
 import HBS2.Net.Proto.Service
 import HBS2.Net.Proto.Sessions
 
 import HBS2.Base58
 import HBS2.Data.Types.Peer
+import HBS2.Data.Types.Refs
 import HBS2.Actors.Peer
 import HBS2.Peer.Proto.Peer
+import HBS2.Peer.Proto.BlockInfo
 import HBS2.Clock
 import HBS2.Net.Auth.Schema
 
@@ -39,11 +42,14 @@ import PeerInfo
 
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Cont
+import Control.Concurrent.STM (flushTQueue)
 import Data.Text qualified as Text
 import Data.Either
 import Data.Maybe
+import Data.Coerce
 import Numeric
 import UnliftIO
+import Streaming.Prelude qualified as S
 
 instance (e ~ L4Proto, MonadUnliftIO m, HasRpcContext PeerAPI RPC2Context m) => HandleMethod m RpcRunScript where
   handleMethod top = do
@@ -84,6 +90,43 @@ instance (e ~ L4Proto, MonadUnliftIO m, HasRpcContext PeerAPI RPC2Context m) => 
             pure $ mkList [mkSym "kicked", mkSym (show $ pretty peer) ]
 
           _ -> pure nil
+
+
+        entry $ bindMatch "request-block-size" \case
+          [LitScientificVal w,  HashLike blk] -> do
+
+            let h = coerce blk
+
+            liftIO $ withPeerM rpcPeerEnv do
+
+              answ <- newTQueueIO
+
+              forKnownPeers @e $ \p _ -> do
+
+                subscribe @e (BlockSizeEventKey p) $ \case
+                  BlockSizeEvent (that, hx, sz) | hx == h -> do
+                    debug $ "FUCKING GOT BLOCK SIZE!" <+> pretty (HashRef hx) <+> pretty p
+                    atomically $ writeTQueue answ (sz, that)
+
+                  _ -> none
+
+                request p (GetBlockSize @e h)
+
+              pause @'Seconds (realToFrac w)
+
+              r <- atomically do
+                     x <- readTQueue answ
+                     xs <- flushTQueue answ
+                     pure (x:xs)
+
+              rr <- S.toList_ $ for_ r $ \(s,p) -> do
+                      S.yield $ mkList @C [ mkSym "size", mkInt s, mkSym (show $ pretty p) ]
+
+              debug $ "WTF?!" <+> pretty rr
+              pure $ mkList rr
+
+          _ -> do
+            pure nil
 
         entry $ bindMatch "peer-info" $ const do
 
