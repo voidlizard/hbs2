938a676baf10a4abe6b62fe2d57810fb8e50c278
voidlizard <dzuikov@gmail.com>
2024-11-22 08:33:45 +0300

    wip

--- hbs2-git3/lib/HBS2/Git3/State/Direct.hs
+++ hbs2-git3/lib/HBS2/Git3/State/Direct.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module HBS2.Git3.State.Direct
   ( module HBS2.Git3.State.Direct
   , module HBS2.Git3.State.Types
@@ -5,13 +6,17 @@ module HBS2.Git3.State.Direct
 
 import HBS2.Prelude.Plated
 import HBS2.OrDie
+import HBS2.Data.Types.Refs
 import HBS2.System.Dir
+
+import HBS2.Git.Local
 
 import HBS2.Git3.State.Types
 
 import DBPipe.SQLite
 
 import System.Directory
+import Data.Maybe
 
 import Text.InterpolatedString.Perl6 (qc)
 
@@ -38,14 +43,41 @@ evolveState = do
 
     ddl [qc|
 create table if not exists
-gitobject
-  ( githash text not null primary key
-  , type    text not null
-  , cblock  text not null
+gitpack
+  ( kommit  text not null primary key
   , pack    text not null
   )
 |]
 
-    pure ()
+
+instance ToField GitHash where
+  toField h = toField (show $ pretty h)
+
+instance ToField GitRef where
+  toField h = toField (show $ pretty h)
+
+instance FromField GitRef where
+  fromField = fmap fromString . fromField @String
+
+instance FromField GitHash where
+  fromField = fmap fromString . fromField @String
+
+instance ToField HashRef where
+  toField x = toField $ show $ pretty x
+
+instance FromField HashRef where
+  fromField = fmap (fromString @HashRef) . fromField @String
+
+insertGitPack :: MonadIO m => GitHash -> HashRef -> DBPipeM m ()
+insertGitPack co pack = do
+  insert [qc|
+    insert into gitpack (kommit,pack) values(?,?)
+    on conflict (kommit) do update set pack = excluded.pack
+         |] (co, pack)
+
+selectGitPack :: MonadIO m => GitHash -> DBPipeM m (Maybe HashRef)
+selectGitPack gh = do
+  select [qc|select pack from gitpack where kommit = ? limit 1|] (Only gh)
+   <&> listToMaybe . fmap fromOnly
 
 
