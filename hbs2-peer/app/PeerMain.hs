a7259e38e8cc576aa912a00ae610284e7a8809cd
voidlizard <dzuikov@gmail.com>
2025-05-27 18:15:34 +0300

    maybe fixed TCP loop on error
    Squashed commit of the following:
    
    commit 0f5a491e34cdcab7155f763fcaf5437937e20dda
    Author: voidlizard <dzuikov@gmail.com>
    Date:   Mon May 26 18:10:11 2025 +0300
    
        maybe fixed TCP loop on error -- additional probe
    
    commit 515d7d65734f4d0a3e1bedfb24dd55fdf0aade98
    Author: voidlizard <dzuikov@gmail.com>
    Date:   Mon May 26 18:08:17 2025 +0300
    
        maybe fixed TCP loop on error

--- hbs2-peer/app/PeerMain.hs
+++ hbs2-peer/app/PeerMain.hs
@@ -1,4 +1,3 @@
-
 {-# Language TemplateHaskell #-}
 {-# Language AllowAmbiguousTypes #-}
 {-# Language UndecidableInstances #-}
@@ -921,12 +920,13 @@ runPeer opts = respawnOnError opts $ do
                   addProbe tcpProbe
                   messagingTCPSetProbe tcpEnv tcpProbe
 
-                void $ liftIO ( async do
+                void $ liftIO ( asyncLinked do
                           runMessagingTCP tcpEnv
                             `U.withException` \(e :: SomeException) -> do
                             err (viaShow e)
                             err "!!! TCP messaging stopped"
                             liftIO $ atomically $ modifyTVar msgAlive pred
+                            throwIO e
                        )
                 let tcpaddr = view tcpOwnPeer tcpEnv
                 liftIO $ atomically $ modifyTVar msgAlive succ
